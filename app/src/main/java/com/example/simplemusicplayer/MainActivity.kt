d1a4c236ba30abfab99ffea9d8d2eb47848ce88b
Artyom <artemrule@gmail.com>
2024-02-22 09:49:17 +0300

    empty comit

--- app/src/main/java/com/example/simplemusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/simplemusicplayer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simplemusicplayer
 
+
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -15,12 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -33,3 +31,5 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 }
+
+//i want jetpack compose pls
