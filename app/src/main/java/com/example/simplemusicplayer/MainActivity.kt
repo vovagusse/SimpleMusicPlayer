1ccbb8d44f959f5637c9fbfc0d8ba2aab45215d5
Artyom <artemrule@gmail.com>
2024-03-10 17:45:19 +0300

    uh nothin

--- app/src/main/java/com/example/simplemusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/simplemusicplayer/MainActivity.kt
@@ -19,12 +19,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navView: BottomNavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController = findNavController(
+            R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -32,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-fun square(x: Float) : Float {
-    return x * x
+
+fun controls(){
+
 }
-//i want jetpack compose pls
